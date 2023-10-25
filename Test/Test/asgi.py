eb1f7a4e92431bcb3a9d21546dfac446b574c34f
Roman Jurkovič <98027196+Knight20102010@users.noreply.github.com>
2023-10-25 09:19:09 +0200

    Update asgi.py

--- Test/Test/asgi.py
+++ Test/Test/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+#why you are so cruel
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test.settings')
