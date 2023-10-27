89e7f8f2f406fb2d09791edb0378bcab325bc1c9
Roman Jurkovič <98027196+Knight20102010@users.noreply.github.com>
2023-10-27 10:59:43 +0200

    d
    d

--- Test/quality/urls.py
+++ Test/quality/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from quality import views
 
 urlpatterns = [
-     path('',views.aboutUs),
-     path('aboutUs/', views.aboutUs)
+     path("",views.hello)
 ]
+     
--- Test/quality/views.py
+++ Test/quality/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os, time
 
 def aboutUs(request):
-    return HttpResponse("Hello guys , this is how we do it")
+    return render(request, 'hello.html')
+
+"""
+employee = {"1123":"Jozef Petras","1124":"Rastislav Guláš","1125":"Mária Surová", "1126":"Júlia Kocáková"}
+
+def looking_name():
+    x = input("Zadaj cislo ID > ")
+    if x in employee:
+        return HttpResponse(employee[x])
+    else:
+        return HttpResponse("ID číslo sa nenachádza v zozname!!!")
+    
+def adding_name():
+    y = input("Zadaj ID cislo > ")
+    z = input("Zadaj zamestnanca > ")  
+    if y in employee:
+        return "ID je už vytvorené!!!"
+    else:    
+        u = employee[y] = z 
+        return u
+  
 
 
-
+while True:
+    time.sleep(2)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    menu = input(("1. View \n2. Add\n>"))
+    if menu == "1":
+        print(looking_name())
+    elif menu == "2":
+        print(adding_name())  
+    else:
+        print("Vyber si z ponuky 1 - View alebo 2 - Add")    
+"""
 
 # Create your views here.
