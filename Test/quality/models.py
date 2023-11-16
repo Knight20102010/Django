deea54f65976a5c552f09ab0bf9c2c47a8b7001a
Roman Jurkovič <98027196+Knight20102010@users.noreply.github.com>
2023-11-16 14:51:35 +0100

    creating model

--- Test/quality/models.py
+++ Test/quality/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250,unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name="post_author")
